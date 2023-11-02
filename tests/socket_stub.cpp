a93133d172148b07fc4273b7cbb4a1d947dea696
Artur Harasimiuk <artur.harasimiuk@intel.com>
2023-11-02 09:51:13 +0000

    ci: infra revision update
    Related-To: NEO-9248
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- tests/socket_stub.cpp
+++ tests/socket_stub.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Intel Corporation
+ * Copyright (C) 2022-2023 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -10,32 +10,38 @@ typedef struct sockaddr SOCKADDR;
 typedef int LPWSADATA;
 typedef struct addrinfo ADDRINFO;
 
+#ifdef _WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int closesocket(SOCKET s) { return 0; }
-int close(SOCKET s) { return 0; }
-int connect(SOCKET s, const struct sockaddr *name, int namelen) { return 0; }
-unsigned short htons(unsigned short hostlong) { return 0; }
-unsigned long inet_addr(const char *cp) { return 0; }
-int recv(SOCKET s, char *buf, int len, int flags) { return 0; }
-int send(SOCKET s, const char *buf, int len, int flags) { return len; }
-int shutdown(SOCKET s, int how) { return 0; }
-SOCKET socket(int af, int type, int protocol) { return 0; }
-struct hostent *gethostbyname(const char *name) {
+EXPORT int closesocket(SOCKET s) { return 0; }
+EXPORT int close(SOCKET s) { return 0; }
+EXPORT int connect(SOCKET s, const struct sockaddr *name, int namelen) { return 0; }
+EXPORT unsigned short htons(unsigned short hostlong) { return 0; }
+EXPORT unsigned long inet_addr(const char *cp) { return 0; }
+EXPORT int recv(SOCKET s, char *buf, int len, int flags) { return 0; }
+EXPORT int send(SOCKET s, const char *buf, int len, int flags) { return len; }
+EXPORT int shutdown(SOCKET s, int how) { return 0; }
+EXPORT SOCKET socket(int af, int type, int protocol) { return 0; }
+EXPORT struct hostent *gethostbyname(const char *name) {
     return nullptr;
 }
-int WSAStartup(int wVersionRequired, LPWSADATA lpWSAData) { return 0; }
-int WSACleanup(void) { return 0; }
-void WSASetLastError(int iError) { return; }
-int WSAGetLastError(void) { return 0; }
+EXPORT int WSAStartup(int wVersionRequired, LPWSADATA lpWSAData) { return 0; }
+EXPORT int WSACleanup(void) { return 0; }
+EXPORT void WSASetLastError(int iError) { return; }
+EXPORT int WSAGetLastError(void) { return 0; }
 
-int getaddrinfo(const char *name, const char *service, const ADDRINFO *hint, ADDRINFO **results) {
+EXPORT int getaddrinfo(const char *name, const char *service, const ADDRINFO *hint, ADDRINFO **results) {
     *results = nullptr;
     return 0;
 }
-void freeaddrinfo(ADDRINFO *info) { return; }
+EXPORT void freeaddrinfo(ADDRINFO *info) { return; }
 
 #ifdef __cplusplus
 }
